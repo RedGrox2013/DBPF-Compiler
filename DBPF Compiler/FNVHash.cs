1cfc0f0806c9bded7babfcde578199827b0f51c6
Барсик Рыжий <ryzijbarsik95@gmail.com>
2024-03-22 16:01:36 +0300

    Добавлено вычисление FNV хешей

---
+++ DBPF Compiler/FNVHash.cs
@@ -0,0 +1,21 @@
+﻿namespace DBPF_Compiler
+{
+    public static class FNVHash
+    {
+        private const uint FNV_32_PRIME = 0x01000193;
+        private const uint FNV_BASIS = 0x811c9dc5;
+
+        public static uint Compute(string input)
+        {
+            uint hash = FNV_BASIS;
+
+            foreach (var c in input.ToLowerInvariant())
+            {
+                hash *= FNV_32_PRIME;
+                hash ^= c;
+            }
+
+            return hash;
+        }
+    }
+}
--- DBPF Compiler/Program.cs
+++ DBPF Compiler/Program.cs
@@ -1,4 +1,5 @@
-﻿using DBPF_Compiler.DBPF;
+﻿using DBPF_Compiler;
+using DBPF_Compiler.DBPF;
 using System.Text;
 
 Console.WriteLine("Database Packed File Compiler");
@@ -12,6 +13,11 @@ Console.WriteLine("Database Packed File Compiler");
 //else
 //    path = args[0];
 
+const string STR_DATA = "Кто прочитал, тот лох";
+byte[] data = Encoding.Default.GetBytes(STR_DATA);
+uint dataID = FNVHash.Compute(STR_DATA);
+Console.WriteLine("0x" + Convert.ToString(dataID, 16));
+
 using FileStream fs = File.Create("data.package");
 using DatabasePackedFile dbpf = new(fs);
-dbpf.WriteData(Encoding.Default.GetBytes("Кто прочитал, тот лох"), 0xBC2C0BE9, 0xBC2C0BE9, 0xBC2C0BE9);
+dbpf.WriteData(data, dataID, dataID, dataID);
