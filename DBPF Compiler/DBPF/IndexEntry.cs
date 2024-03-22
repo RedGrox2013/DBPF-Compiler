9b1c5d40f64b01f36aa74951813b4c9c3ab85ff8
Барсик Рыжий <ryzijbarsik95@gmail.com>
2024-03-22 14:20:44 +0300

    Update IndexEntry.cs

--- DBPF Compiler/DBPF/IndexEntry.cs
+++ DBPF Compiler/DBPF/IndexEntry.cs
@@ -2,10 +2,10 @@
 {
     internal struct IndexEntry
     {
-        public uint TypeId { get; set; }
-        public uint GroupId { get; set; }
+        public uint TypeID { get; set; }
+        public uint GroupID { get; set; }
         public readonly uint? UnknownID = null;
-        public uint InstanceId { get; set; }
+        public uint InstanceID { get; set; }
         public uint Offset { get; set; }
         public uint CompressedSize { get; set; }
         public uint UncompressedSize { get; set; }
