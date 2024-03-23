b3e1db79936fb5b136c2e31d7bd52cd014774d2c
Барсик Рыжий <ryzijbarsik95@gmail.com>
2024-03-24 02:42:01 +0300

    Добавил поддержку необычных индексов

--- DBPF Compiler/DBPF/DBPFIndex.cs
+++ DBPF Compiler/DBPF/DBPFIndex.cs
@@ -2,10 +2,43 @@
 {
     internal struct DBPFIndex
     {
-        public readonly uint ValuesFlag = 4;
-        public readonly uint? TypeID = null;
-        public readonly uint? GroupID = null;
-        public const uint UnknownID = 0;
+        public uint ValuesFlag { get; set; } = 4;
+        public uint? TypeID
+        {
+            readonly get => GetFlagAt(0) ? _typeID : null;
+            set => _typeID = value ?? 0;
+        }
+        public uint? GroupID
+        {
+            readonly get => GetFlagAt(1) ? _groupID : null;
+            set => _groupID = value ?? 0;
+        }
+        public uint? UnknownID
+        {
+            readonly get => GetFlagAt(2) ? _unknownID : null;
+            set => _unknownID = value ?? 0;
+        }
+
+        private uint _typeID, _groupID, _unknownID;
+
+        public readonly int SizeWithoutEntries
+        {
+            get
+            {
+                int size = sizeof(uint);
+                if (GetFlagAt(0))
+                    size += sizeof(uint);
+                if (GetFlagAt(1))
+                    size += sizeof(uint);
+                if (GetFlagAt(2))
+                    size += sizeof(uint);
+
+                return size;
+            }
+        }
+
+        public readonly bool GetFlagAt(int bitIndex)
+            => ((ValuesFlag >> bitIndex) & 0b0001) == 1;
 
         public List<IndexEntry> Entries { get; private set; }
 
--- DBPF Compiler/DBPF/IndexEntry.cs
+++ DBPF Compiler/DBPF/IndexEntry.cs
@@ -2,16 +2,30 @@
 {
     public class IndexEntry
     {
-        public uint TypeID { get; set; }
-        public uint GroupID { get; set; }
-        public readonly uint? UnknownID = null;
+        public uint? TypeID { get; set; } = null;
+        public uint? GroupID { get; set; } = null;
+        public uint? UnknownID { get; set; } = null;
         public uint InstanceID { get; set; }
         public uint Offset { get; set; }
         public uint CompressedSize { get; set; }
         public uint UncompressedSize { get; set; }
         public bool IsCompressed { get; set; } = false;
-        public bool IsSaved { get; private set; } = true;
+        public bool IsSaved { get; set; } = true;
 
-        public const int EntrySize = 4 + 4 + 4 + 4 + 2 + 2 + sizeof(uint) + sizeof(uint);
+        public int EntrySize
+        {
+            get
+            {
+                int size = sizeof(uint) * 4 + sizeof(ushort) * 2;
+                if (TypeID != null)
+                    size += sizeof(uint);
+                if (GroupID != null)
+                    size += sizeof(uint);
+                if (UnknownID != null)
+                    size += sizeof(uint);
+
+                return size;
+            }
+        }
     }
 }
