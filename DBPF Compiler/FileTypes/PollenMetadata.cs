f890d083e7bf1a1bae2542867ea126b988c3ca99
Барсик Рыжий <ryzijbarsik95@gmail.com>
2024-03-31 03:01:06 +0300

    Добавлен PollenMetadata.cs

---
+++ DBPF Compiler/FileTypes/PollenMetadata.cs
@@ -0,0 +1,69 @@
+﻿using DBPF_Compiler.Types;
+
+namespace DBPF_Compiler.FileTypes
+{
+    public class PollenMetadata : ISporeFile
+    {
+        public uint TypeID => 0x030BDEE3;
+
+        public int MetadataVersion { get; set; } = 13;
+        public ulong AssetID { get; set; }
+        public ResourceKey AssetKey { get; set; }
+        public ResourceKey ParentAssetKey { get; set; } = new(0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF);
+        public long ParentAssetID { get; set; } = 0xFFFFFFFF;
+        public long OriginalParentAssetID { get; set; } = 0xFFFFFFFF;
+        public ulong TimeCreated { get; set; } = 0;
+        public ulong TimeDownloaded { get; set;} = 0;
+
+        /*
+         * Дальше идут 4 байта:
+         * 0xFFFFFFFF - если есть локализации
+         * 0 - если их нет
+         */
+
+        /// <summary>
+        /// Имеется только если нет локализаций
+        /// </summary>
+        public long? AuthorID { get; set; } = 0xFFFFFFFF;
+
+        /// <summary>
+        /// Имеется только если нет локализаций
+        /// </summary>
+        public string? AuthorName { get; set; }
+
+        /// <summary>
+        /// Имеется только если нет локализаций
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Имеется только если нет локализаций
+        /// </summary>
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// Имеется только если есть локализации
+        /// </summary>
+        public uint LocaleTableID { get; set; }
+
+        /// <summary>
+        /// Имеется только если есть локализации
+        /// </summary>
+        public uint AuthorNameLocale { get; set; }
+
+        /// <summary>
+        /// Имеется только если есть локализации
+        /// </summary>
+        public uint DescriptionLocale { get; set; }
+
+        public bool Decode(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Encode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
