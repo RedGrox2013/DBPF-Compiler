963e6cbc776bf36f14731390bf2d0c4fc8834225
Барсик Рыжий <ryzijbarsik95@gmail.com>
2024-04-26 15:14:58 +0300

    Начал реализовывать чтение prop-файлов

---
+++ DBPF Compiler/FileTypes/Prop/Property.cs
@@ -0,0 +1,17 @@
+﻿namespace DBPF_Compiler.FileTypes.Prop
+{
+    public struct Property
+    {
+        public string Name { get; set; }
+        public PropertyType PropertyType { get; set; }
+        public object Value { get; set; }
+    }
+
+    public enum PropertyType
+    {
+        Boolean = 0x10000,
+        Int8 = 0x50000,
+        Int16 = 0x70000,
+        Int32 = 0x90000,
+    }
+}
---
+++ DBPF Compiler/FileTypes/Prop/PropertyList.cs
@@ -0,0 +1,19 @@
+﻿using DBPF_Compiler.FNV;
+
+namespace DBPF_Compiler.FileTypes.Prop
+{
+    public class PropertyList : ISporeFile
+    {
+        public uint TypeID => 0x00B1B104;
+
+        public bool Decode(byte[]? data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public uint WriteToStream(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
