db9034d537686eba44a4d1880017a7db878ee05c
Барсик Рыжий <ryzijbarsik95@gmail.com>
2024-05-24 22:38:45 +0300

    Небольшие изменения

--- DBPF Compiler/DBPF/DBPFPacker.cs
+++ DBPF Compiler/DBPF/DBPFPacker.cs
@@ -6,7 +6,14 @@ namespace DBPF_Compiler.DBPF
 {
     public class DBPFPacker
     {
+        public delegate void DBPFPackerEventHandler(object? sender, StringResourceKey key);
+
         public DirectoryInfo UnpackedDataDirectory { get; set; }
+
+        private DBPFPackerEventHandler? _packHandler = null;
+        public event DBPFPackerEventHandler? PackHandler { add => _packHandler += value; remove => _packHandler -= value; }
+        private DBPFPackerEventHandler? _unpackHandler = null;
+        public event DBPFPackerEventHandler? UnpackHandler { add => _unpackHandler += value; remove => _unpackHandler -= value; }
 
         public DBPFPacker(DirectoryInfo unpackedDataDirectory)
             => UnpackedDataDirectory = unpackedDataDirectory;
@@ -25,6 +32,7 @@ namespace DBPF_Compiler.DBPF
                     foreach (var secret in group.GetFiles())
                     {
                         StringResourceKey key = new(secret.Name, secret.Extension.Remove(0, 1));
+                        _packHandler?.Invoke(this, key);
                         using FileStream f = secret.OpenRead();
                         output.CopySecretDataFromStream(f, key);
                     }
@@ -37,7 +45,10 @@ namespace DBPF_Compiler.DBPF
                     if (!d.Name.EndsWith(".package.unpacked"))
                         continue;
 
-                    DBPFPacker packer = new(d);
+                    DBPFPacker packer = new(d)
+                    {
+                        _packHandler = _packHandler
+                    };
                     using MemoryStream stream = new();
                     using DatabasePackedFile package = new(stream);
                     packer.Pack(package);
@@ -57,6 +68,7 @@ namespace DBPF_Compiler.DBPF
                     string[] splitFileName = file.Name.Split('.');
                     string fileName = splitFileName[0];
                     string extension = splitFileName.Length > 1 ? splitFileName[1] : string.Empty;
+                    _packHandler?.Invoke(this, new(fileName, extension, group.Name));
                     ResourceKey key = _regManager.GetResourceKey(fileName, extension, group.Name);
 
                     if (file.Name.EndsWith(".prop.json", StringComparison.InvariantCultureIgnoreCase) ||
@@ -87,6 +99,7 @@ namespace DBPF_Compiler.DBPF
                 if (!Directory.Exists(path))
                     Directory.CreateDirectory(path);
                 path += "\\" + key.InstanceID + "." + key.TypeID;
+                _unpackHandler?.Invoke(this, key);
 
                 if (resource.TypeID == (uint)TypeIDs.package && flags.HasFlag(EncodeFlags.Package))
                 {
@@ -138,6 +151,12 @@ namespace DBPF_Compiler.DBPF
             return true;
         }
     }
+
+    //public class DBPFPackerEventArgs
+    //{
+    //    public StringResourceKey CurrentKey { get; set; }
+    //    public int KeysCount { get; set; }
+    //}
 
     [Flags]
     public enum EncodeFlags
