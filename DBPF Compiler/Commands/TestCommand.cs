c10f7ff339320d6fd50a4e84e2f4b4bf4d21410f
Барсик Рыжий <ryzijbarsik95@gmail.com>
2024-08-01 18:38:02 +0300

    Экспериментировал с красивым выводом для команды keys

--- DBPF Compiler/Commands/TestCommand.cs
+++ DBPF Compiler/Commands/TestCommand.cs
@@ -1,4 +1,6 @@
 ﻿using DBPF_Compiler.ArgScript;
+using DBPF_Compiler.DBPF;
+using DBPF_Compiler.FNV;
 using DBPF_Compiler.Types;
 
 namespace DBPF_Compiler.Commands
@@ -7,11 +9,94 @@ namespace DBPF_Compiler.Commands
     {
         public override void ParseLine(Line line)
         {
-            StringResourceKey key = StringResourceKey.Parse(line[1]);
-            WriteLine(
-@$"Group:    {key.GroupID}
-Instance: {key.InstanceID}
-Type:     {key.TypeID}");
+            if (line.ArgumentCount < 2)
+            {
+                PrintError("Missing <filePath> argument.");
+                return;
+            }
+
+            using FileStream file = File.OpenRead(line[1]);
+            using DatabasePackedFile dbpf = new(file);
+            var keys = dbpf.ReadDBPFInfo();
+
+            if (!line.HasFlag("-no-header"))
+                WriteLine($@"Database packed file header:
+Major version: {dbpf.MajorVersion}
+Index count:   {dbpf.IndexCount}
+Index size:    {dbpf.IndexSize} bytes
+Index offset:  {dbpf.IndexOffset} bytes
+
+Resources:");
+
+            if (!line.HasFlag("b"))
+            {
+                WriteLine(Path.GetFileName(file.Name));
+                PrintTree(dbpf, keys);
+                return;
+            }
+
+            foreach (var key in keys)
+                WriteLine(NameRegistryManager.Instance.GetStringResourceKey(key));
+        }
+
+        private void PrintTree(DatabasePackedFile dbpf, IEnumerable<ResourceKey> keys, int level = 1)
+        {
+            Dictionary<string, List<ResourceKey>> groups = [];
+            foreach (var key in keys)
+            {
+                string groupID = NameRegistryManager.Instance.GetName(key.GroupID, "file");
+                if (!groups.TryGetValue(groupID, out var group))
+                {
+                    group = [];
+                    groups.Add(groupID, group);
+                }
+                group.Add(key);
+            }
+
+            for (int i = 0; i < groups.Count; i++)
+            {
+                for (int k = 1; k < level; k++)
+                    Write(" | ");
+                if (level > 1)
+                    Write(" | ");
+
+                var group = groups.ElementAt(i);
+                if (i < groups.Count - 1)
+                    Write(" ├ ");
+                else
+                    Write(" └ ");
+                WriteLine(group.Key);
+
+                for (int j = 0; j < group.Value.Count; j++)
+                {
+                    for (int k = 0; k < level; k++)
+                    {
+                        Write(" | ");
+                    }
+                    if (level > 1)
+                        Write(" | ");
+
+                    if (j < group.Value.Count - 1)
+                        Write(" ├ ");
+                    else
+                        Write(" └ ");
+                    
+                    string name = NameRegistryManager.Instance.GetName(group.Value[j].InstanceID, "file");
+                    uint typeID = group.Value[j].TypeID;
+                    if (typeID != 0)
+                        name += "." + NameRegistryManager.Instance.GetName(typeID, "file");
+
+                    WriteLine(name);
+
+                    if (typeID == (uint)TypeIDs.package)
+                    {
+                        using MemoryStream stream = new();
+                        dbpf.CopyResourceTo(stream, group.Value[j]);
+                        using DatabasePackedFile package = new(stream);
+                        PrintTree(package, package.ReadDBPFInfo(), level + 1);
+                    }
+                }
+            }
         }
     }
 }
