8195c9bb94a06c33aee26e09e2b0823519b00393
Барсик Рыжий <ryzijbarsik95@gmail.com>
2024-08-03 21:28:14 +0300

    Добавлен ModProject.cs

---
+++ DBPF Compiler/ModProject.cs
@@ -0,0 +1,15 @@
+﻿using DBPF_Compiler.DBPF;
+using System.Text.Json.Serialization;
+
+namespace DBPF_Compiler
+{
+    public class ModProject
+    {
+        [JsonIgnore]
+        public DBPFPacker? Packer { get; set; }
+        [JsonIgnore]
+        public string? Path => Packer?.UnpackedDataDirectory.FullName;
+
+        public ModProject() { }
+    }
+}
