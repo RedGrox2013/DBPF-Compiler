202e8141dc5f9339c3e3766124ef36ba2980e915
Барсик Рыжий <ryzijbarsik95@gmail.com>
2024-09-29 17:47:25 +0300

    Добавлена команда create-template

--- DBPF Compiler/Commands/CreateTemplateCommand.cs
+++ DBPF Compiler/Commands/CreateTemplateCommand.cs
@@ -1,9 +1,5 @@
 ﻿using DBPF_Compiler.ArgScript;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DBPF_Compiler.ModsTemplates;
 
 namespace DBPF_Compiler.Commands
 {
@@ -11,7 +7,52 @@ namespace DBPF_Compiler.Commands
     {
         public override void ParseLine(Line line)
         {
-            throw new NotImplementedException();
+            string? projectPath = line.GetOption("p", 1)?[0];
+            if (string.IsNullOrEmpty(projectPath))
+            {
+                PrintError("Required argument missing: <projectPath>");
+                return;
+            }
+
+            string? type = line.GetOption("t", 1)?[0]?.ToLower();
+            if (string.IsNullOrEmpty(type))
+            {
+                PrintError("Required argument missing: <type>");
+                return;
+            }
+
+            var project = ModProject.Deserialize(projectPath);
+            switch (type)
+            {
+                case "music":
+                    string? folder = line.GetOption("-folder", 1)?[0];
+                    if (string.IsNullOrEmpty(folder))
+                    {
+                        // доделать
+                        throw new NotImplementedException("Specify the folder with music");
+                    }
+
+                    foreach (var file in Directory.GetFiles(folder))
+                    {
+                        var extension = Path.GetExtension(file);
+                        if (!extension.Equals(".mp3", StringComparison.OrdinalIgnoreCase) &&
+                            !extension.Equals(".snr", StringComparison.OrdinalIgnoreCase))
+                            continue;
+
+                        var template = new MusicModTemplate() { FileName = file };
+                        string thumb = Path.GetFileNameWithoutExtension(file) + ".png";
+                        if (File.Exists(thumb))
+                            template.PlannerThumbnail = thumb;
+
+                        project.AddModTemplate(template);
+                    }
+                    break;
+                default:
+                    PrintError("Unknown template: " + type);
+                    return;
+            }
+
+            WriteLine(project.Name + ":\n" + ModProject.Serialize(project, projectPath));
         }
     }
 }
