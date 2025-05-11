c8440200d65ef264ea1b6ece03fd3d0a514205b4
Барсик Рыжий <ryzijbarsik95@gmail.com>
2025-05-11 17:47:16 +0300

    Настройка Lua вынесена в отдальный скрипт

--- DBPF Compiler/DBPFCLua/LuaCreator.cs
+++ DBPF Compiler/DBPFCLua/LuaCreator.cs
@@ -1,5 +1,6 @@
 ﻿using DBPF_Compiler.FNV;
 using NLua;
+using System.Reflection;
 
 namespace DBPF_Compiler.DBPFCLua;
 
@@ -27,47 +28,17 @@ public class LuaCreator(TraceConsole console)
 
         lua.DoString(@$"package.path = package.path ..
     "";{Path.Combine(Directory.GetCurrentDirectory(), "scripts", "?.lua").Replace("\\", "\\\\")}""
-
-local versionmt = {{
-    __tostring = function(self)
-        return self.major .. '.' .. self.minor .. '.' .. self.build .. '.' .. self.revision
-    end
-}}
-versionmt.__index = versionmt
-DBPFC_VERSION = setmetatable({{}}, versionmt)
-
-local _trace = __trace__
-local _write = __write__
-local _hash  = __hash__
-__trace__, __write__, __hash__ = nil, nil, nil
-function write(v) _write(tostring(v)) end
-function hash (v) return _hash (tostring(v)) end
-function trace(...)
-    local args = table.pack(...)
-    if args.n == 1 then
-        _trace(tostring(args[1]))
-        return
-    end
-
-    for i = 1, args.n do
-        write(args[i])
-        _write('\t')
-    end
-    _write('\n')
-end
-
--- для лучшей совместимости с DBPFC некоторые функции будут переопределены
-print    = trace
-io.write = write
-io.read  = readline
-os.exit  = nil -- добавить потом норм метод
-
--- чтобы не было путаницы с регистром имён функций
-readLine   = readline
-hashToName = hashtoname
 ", "configuration");
 
-        var version = GetType().Assembly.GetName().Version;
+        var assembly = Assembly.GetExecutingAssembly();
+        using var stream = assembly.GetManifestResourceStream("DBPF_Compiler.scripts.initLua.lua");
+        if (stream != null)
+        {
+            using var reader = new StreamReader(stream);
+            lua.DoString(reader.ReadToEnd());
+        }
+
+        var version = assembly.GetName().Version;
         lua["DBPFC_VERSION.major"]    = version?.Major;
         lua["DBPFC_VERSION.minor"]    = version?.Minor;
         lua["DBPFC_VERSION.build"]    = version?.Build;
---
+++ DBPF Compiler/scripts/initLua.lua
@@ -0,0 +1,37 @@
+-- Настройка Lua для DBPFC
+
+local versionmt = {
+    __tostring = function(self)
+        return self.major .. '.' .. self.minor .. '.' .. self.build .. '.' .. self.revision
+    end
+}
+versionmt.__index = versionmt
+DBPFC_VERSION = setmetatable({}, versionmt)
+
+local _trace, _write, _hash = __trace__, __write__, __hash__
+__trace__, __write__, __hash__ = nil, nil, nil
+function write(v) _write(tostring(v)) end
+function hash (v) return _hash (tostring(v)) end
+function trace(...)
+    local args = table.pack(...)
+    if args.n == 1 then
+        _trace(tostring(args[1]))
+        return
+    end
+
+    for i = 1, args.n do
+        write(args[i])
+        _write('\t')
+    end
+    _write('\n')
+end
+
+-- для лучшей совместимости с DBPFC некоторые функции будут переопределены
+print    = trace
+io.write = write
+io.read  = readline
+os.exit  = nil -- добавить потом норм метод
+
+-- чтобы не было путаницы с регистром имён функций
+readLine   = readline
+hashToName = hashtoname
