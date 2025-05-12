864dedc53e67d17ddd5aba29c9e6a9d8d9a8b7c5
Барсик Рыжий <ryzijbarsik95@gmail.com>
2025-05-12 13:15:43 +0300

    Небольшие изменения

--- DBPF Compiler/scripts/classes/Property.lua
+++ DBPF Compiler/scripts/classes/Property.lua
@@ -3,8 +3,8 @@ Property.__index = Property
 
 function Property.new(propType, value)
 	return setmetatable({
-		PropertyType = propType,
-		Value = value
+		propertyType = string.lower(tostring(propType)),
+		value = value
 	}, Property)
 end
 
@@ -28,10 +28,41 @@ function Property.newString16(value)
 end
 function Property.newKey(instanceId, typeId, groupId)
 	if instanceId.className == "Key" then
-		return Property.new("key", instanceId)
+		return Property.new(instanceId.className, instanceId)
 	end
 
 	return Property.new("key", Key.new(instanceId, typeId, groupId))
 end
+function Property.newVector2(x, y)
+	if x.className == "Vector2" then
+		return Property.new(x.className, x)
+	end
 
--- äîäåëàòü!
+	return Property.new("vector2", Vector2.new(x, y))
+end
+
+-- Ð´Ð¾Ð´ÐµÐ»Ð°Ñ‚ÑŒ!
+
+function Property:__tostring()
+	if type(self.value) == "number" and self.propertyType == "uint32" and self.value > 100000 then
+		return FNVHash.ToString(self.value)
+	end
+	if self.propertyType == "string8" or self.propertyType == "string16" then
+		return '"' .. tostring(self.value) .. '"'
+	end
+	if string.sub(self.propertyType, -1) == "s" then
+		local str = "\n"
+		for _, v in ipairs(self.value) do
+			local s = tostring(v)
+			if self.propertyType == "string8s" or self.propertyType == "string16s" then
+				s = '"' .. s .. '"'
+			end
+
+			str = str .. "\t" .. s .. "\n"
+		end
+
+		return str .. "end"
+	end
+
+	return tostring(self.value)
+end
--- DBPF Compiler/scripts/classes/PropertyList.lua
+++ DBPF Compiler/scripts/classes/PropertyList.lua
@@ -1,4 +1,27 @@
 PropertyList = DBPFCObject.new("PropertyList")
 PropertyList.__index = PropertyList
 
--- доделать
+function PropertyList.new(list)
+    return setmetatable(list or {}, PropertyList)
+end
+
+function PropertyList:add(name, property, value)
+    if property.className == "Property" then
+        self[name] = property
+        return
+    end
+
+    self[name] = Property.new(property, value)
+end
+
+function PropertyList:__tostring()
+    local str = ""
+    for k, v in pairs(self) do
+        if v.className == "Property" then
+            str = str .. v.propertyType .. " " ..
+                tostring(k) .. " " .. tostring(v) .. "\n"
+        end
+    end
+
+    return str
+end
